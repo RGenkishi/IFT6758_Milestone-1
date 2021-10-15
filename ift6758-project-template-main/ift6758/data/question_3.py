e8f7844bc7a10485b4c36bf71776e87ad797b7b3
N-Lefevre-UDEM <nathanael.lefevre@umontreal.ca>
2021-10-14 21:20:14 -0400

    Code mis à jour et déplacé dans question_3.py le notebook ne fait que l'importer

--- ift6758-project-template-main/ift6758/data/question_3.py
+++ ift6758-project-template-main/ift6758/data/question_3.py
@@ -1,39 +1,46 @@
 from ift6758.data.question_4 import Tidyfier
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from PIL import Image
 import ipywidgets as widgets
+from IPython.display import display
 
 
 
 class debugWidget:
 
-    def __init__(self, year):
-        tdf = Tidyfier()
-        self.df = tdf.game_event_to_panda_df(year)
+    def __init__(self):
+        self.tdf = Tidyfier()
+        self.df = None
+        self.subDf = []
 
-    df = df[df['dateYear'] == "2017"]
-    df = df[df['dateMonth'] == "10"]
-    df = df[df['dateDay'] == "04"]
 
-    def f(m, checkWithNhlExemple, checkWithNhlExemple):
-        global df
-        fig,ax = plt.subplots()
+    def drawIceRink(self, i):
+        fig, ax = plt.subplots()
         img = Image.open('../../figures/nhl_rink.png')
-        ax.imshow(img, extent=[-100,100,-43,43])
+        ax.imshow(img, extent=[-100, 100, -43, 43])
         # Add scatter point on the image.
-        imgx,imgy = [float(df.loc[m,"coordX"])], [float(df.loc[m,"coordY"])]
-        ax.scatter(imgx,imgy,s=5,lw=5,facecolor="none",edgecolor="magenta")
+        imgx, imgy = [float(self.subDf.iloc[int(i)]["coordX"])], [float(self.subDf.iloc[int(i)]["coordY"])]
+        ax.scatter(imgx, imgy, s=5, lw=5, facecolor="none", edgecolor="magenta")
 
 
+    def selectMatch(self, m):
+        self.subDf = self.df[self.df["matchId"] == m]
+        # print(subDf)
+        lenDf = len(self.subDf)
+        w = widgets.IntSlider(value=0, min=0, max=lenDf - 1, description='event:', continuous_update=False)
+        interactive_IceRinkPlot = widgets.interactive(self.drawIceRink, i=w)
+        display(interactive_IceRinkPlot)
 
-pd.set_option("max_rows", None)
-pd.set_option("max_columns", None)
-print(df.head(5))
-#print(df.info())
-#print(df.loc[0,"period"])
+
+    def selectYear(self, y):
+        global df
+        self.df = self.tdf.game_event_to_panda_df(y)
+        matchIdList = self.df['matchId'].drop_duplicates()
+        sliderSelectMatch = widgets.interactive(self.selectMatch, m=matchIdList)
+        display(sliderSelectMatch)
 
 
-interactive_plot = widgets.interactive(f, m=(0, len(df)-1), checkWithNhlExemple=(True, False))
-output = interactive_plot.children[-1]
-output.layout.height = '350px'
-interactive_plot
+    def lauchWidget(self):
+        y = widgets.Dropdown(options=range(1917, 2021), value=2017, description='Year:', disabled=False)
+        dropDownYearSelect = widgets.interactive(self.selectYear, y=y)
+        display(dropDownYearSelect)
