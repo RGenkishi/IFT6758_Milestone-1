fe3740ecb74a1da9677c305e73a80a9eefdf759e
RGenkishi <romeogenkishi4@gmail.com>
2021-11-23 03:02:09 -0500

    mise a jour Question 1

--- ift6758-project-template-main/ift6758/data/milestone2/question_1.py
+++ ift6758-project-template-main/ift6758/data/milestone2/question_1.py
@@ -1,9 +1,15 @@
+
 from ift6758.data.question_4 import Tidyer
+from ift6758.data.tidyDataKeys import *
+from ift6758.data.question_6 import HeatMapShots
 import pandas as pd
 import numpy as np
 
+x_net_coordinate = 10
+heatmap_function = HeatMapShots()
+
 class Featurizer():
-    
+
     def __init__(self,period_start,period_end):
         self.period_start = period_start
         self.period_end = period_end
@@ -24,6 +30,18 @@ class Featurizer():
         dist = np.sqrt((x - 90)**2 + (y - 0)**2)
         return dist
 
+    def calculate_distance_from_net(self,season_data):
+        season_data = heatmap_function.rearange_coordinates(season_data)
+        season_data['distance'] = np.sqrt(
+            (season_data.coord_x - x_net_coordinate) ** 2 + season_data.coord_y ** 2)
+        return season_data
+
+    def calculate_angle(self,season_data):
+        season_data["angle"] = np.arccos(
+            (season_data.coord_x - x_net_coordinate) / season_data.distance_from_net)
+        season_data.loc[season_data.coord_y < 0,"angle"] = -season_data.loc[season_data.coord_y < 0,"angle"]
+        return season_data
+
     def get_feature(self):
         df = self.get_all_in_one(self.period_start, self.period_end)
         df['is_goal'] = df['is_goal'].astype(int)
@@ -31,7 +49,10 @@ class Featurizer():
         df = df.assign(angle = 0)
         df = df.assign(filet_vide = 2 )
         feature_df = df[['is_goal','distance','angle','filet_vide']]
-        i=0
+        #feature_df = self.calculate_distance_from_net(feature_df)
+        #feature_df = self.calculate_angle(feature_df)
+
+        '''i=0
         for x,y,side in zip(df['coord_x'],df['coord_y'],df['rink_side']):
 
             if side == 'left' and x >= 0 or side == 'right' and x<= 0:
@@ -41,6 +62,7 @@ class Featurizer():
             else:
                 feature_df['distance'].loc[i] = np.nan
             i +=1
+        '''
         j = 0
         for goalie in df['goalie_name']:
             if goalie is np.nan:
