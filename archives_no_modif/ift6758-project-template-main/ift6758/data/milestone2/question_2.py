44adf0a885b6010fb3580e973ccfa48149237e0f
N-Lefevre-UDEM <nathanael.lefevre@umontreal.ca>
2021-12-01 20:09:35 -0500

    updates mineures

--- archives_no_modif/ift6758-project-template-main/ift6758/data/milestone2/question_2.py
+++ archives_no_modif/ift6758-project-template-main/ift6758/data/milestone2/question_2.py
@@ -11,7 +11,7 @@ exp = Experiment(
 import matplotlib.pyplot as plt
 
 from ift6758.data.tidyer import Tidyer
-from ift6758.data.question_6 import HeatMapShots
+from ift6758.data.heatMapShots import HeatMapShots
 from ift6758.data.tidyDataKeys import *
 import pandas as pd
 import numpy as np
